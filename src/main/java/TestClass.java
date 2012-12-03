904c3748d19882dc4c6d02a92823958157b0acb7
richka <mariianikonova@gmail.com>
2012-12-03 17:52:49 +0200

    testClass

---
+++ src/main/java/TestClass.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mnikonova
+ * Date: 12/3/12
+ * Time: 6:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class TestClass {
+public static void testStart () {
+    System.out.println("Test String");
+}
+
+    public static void main (String [] args){
+        TestClass.testStart ();
+    }
+}
