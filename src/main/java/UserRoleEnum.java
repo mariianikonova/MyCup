e1a08f65638f89c08bb944e7d80ced3a227162cf
richka <mariianikonova@gmail.com>
2013-01-26 00:35:08 +0200

    odd Enum

---
+++ src/main/java/UserRoleEnum.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mnikonova
+ * Date: 1/26/13
+ * Time: 1:34 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserRoleEnum {
+}
