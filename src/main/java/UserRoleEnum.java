722e688d910f8d83fafe7febea5a5231a7976386
richka <mariianikonova@gmail.com>
2013-01-26 00:45:12 +0200

    delete enum

--- src/main/java/UserRoleEnum.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: mnikonova
- * Date: 1/26/13
- * Time: 1:34 AM
- * To change this template use File | Settings | File Templates.
- */
-public class UserRoleEnum {
-}
