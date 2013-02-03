447917a056c9e2c95b4ed132b29b96eb795c18e8
richka <mariianikonova@gmail.com>
2013-02-03 23:43:53 +0200

    Update for AuthFilter

--- src/main/java/AuthFilter.java
+++ src/main/java/AuthFilter.java
@@ -1,6 +1,5 @@
 import javax.servlet.*;
 import javax.servlet.http.*;
-import java.util.ArrayList;
 
 public class AuthFilter implements Filter, CustomSessionAttributes {
 
@@ -9,7 +8,7 @@ public class AuthFilter implements Filter, CustomSessionAttributes {
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws java.io.IOException, ServletException {
         HttpSession session = createSession(request);
-        if (isAuthenticated(request)) {
+        if (isAuthenticated(session)) {
             chain.doFilter(request, response);
         } else {
             String path = ((HttpServletRequest) request).getRequestURI();
@@ -24,19 +23,16 @@ public class AuthFilter implements Filter, CustomSessionAttributes {
     private HttpSession createSession(ServletRequest request) throws java.io.IOException, ServletException {
         HttpSession session = null;
         if (request instanceof HttpServletRequest) {
-            session = ((HttpServletRequest) request).getSession();
+            session = ((HttpServletRequest) request).getSession(true);
         }
         return session;
     }
 
-    private boolean isAuthenticated(ServletRequest request) throws java.io.IOException, ServletException {
-        if (request instanceof HttpServletRequest) {
-            HttpSession session = ((HttpServletRequest) request).getSession(false);
-            if (session != null) {
-                User user = (User) session.getAttribute(SESSION_ATTR_AUTH_USER);
-                if (user != null) {
-                    return true;
-                }
+    private boolean isAuthenticated(HttpSession session) throws java.io.IOException, ServletException {
+        if (session != null) {
+            User user = (User) session.getAttribute(SESSION_ATTR_AUTH_USER);
+            if (user != null) {
+                return true;
             }
         }
         return false;
